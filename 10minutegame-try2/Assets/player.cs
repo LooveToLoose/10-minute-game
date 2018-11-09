64e5add4b1fd083e8556b1aa85358f1f6be8f3da
Jonas Tyroller <tyroller.jonas@gmail.com>
2018-11-09 18:08:53 +0100

    10 min game

---
+++ 10minutegame-try2/Assets/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour {
+
+    Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
+        rigid.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
+        rigid.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ 10minutegame-try2/Assets/spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    public GameObject goSpawn;
+    public float fDifficulty = 40f;
+    float fSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fSpawn += fDifficulty * Time.deltaTime;
+        fDifficulty += 4f * Time.deltaTime;
+        while (fSpawn > 0)
+        {
+            fSpawn -= 1;
+            Vector3 v3Pos = transform.position + new Vector3(Random.value * 40 - 20f, 0, Random.value * 40 - 20f);
+            Quaternion qRot = Quaternion.Euler(0, Random.value * 360f, Random.value * 30f);
+            Vector3 v3Scale = new Vector3(Random.value + 0.1f, 10f, Random.value + 0.1f);
+            GameObject goCreated = Instantiate(goSpawn, v3Pos, qRot);
+            goCreated.transform.localScale = v3Scale;
+        }
+	}
+}
